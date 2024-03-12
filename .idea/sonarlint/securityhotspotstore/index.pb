0560367524e534cc04590e3ea1f6d472b5d0984a
kristine.kaposte <kristine.kaposte@accenture.com>
2024-03-12 10:35:31 +0200

    #1 Creating Invoice Homework Logic

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -1,3 +1,14 @@
 
 =
 src/Main.java,9/2/924f089edb4af5ee45c8a9d1129cd28e2b368138
+@
+src/Invoice.java,3/9/39982de21ebdfd0694b05aec7c6e76e1a815d8d7
+:
+
+.gitignore,a/5/a5cc2925ca8258af241be7e5b0381edf30266302
+S
+#.idea/sonarlint/issuestore/index.pb,9/f/9fe84ebb15faf917b7def6236dba604453cc61e0
+C
+InvoiceHomework.iml,e/8/e8a29d11ae1a3bcb5e76aae3cc727d08123d2586
+]
+-.idea/sonarlint/securityhotspotstore/index.pb,8/8/88a9255124c95bdc913197c120a8d560edc59c8e
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Creating an Invoice object
+        Invoice invoice = new Invoice("1234", "Hammer", 3, 150);
+
+        // Invoice details
+        System.out.println("Part Number: " + invoice.getPartNumber());
+        System.out.println("Part Description: " + invoice.getPartDescription());
+        System.out.println("Quantity: " + invoice.getQuantity());
+        System.out.println("Price per Item: " + invoice.getPricePerItem());
+        System.out.println("Invoice Amount: " + invoice.getInvoiceAmount());
+
+        // Change some attributes
+        invoice.setQuantity(5);
+        invoice.setPricePerItem(100);
+
+        // Updated invoice details
+        System.out.println("\nUpdated Invoice Details:");
+        System.out.println("Quantity: " + invoice.getQuantity());
+        System.out.println("Price per Item: " + invoice.getPricePerItem());
+        System.out.println("Invoice Amount: " + invoice.getInvoiceAmount());
+
+        // Change attributes to negative numbers
+        invoice.setQuantity(-5);
+        invoice.setPricePerItem(-40);
+
+        // Updated invoice details to negative
+        System.out.println("\nUpdated Invoice Details to negative numbers:");
+        System.out.println("Quantity: " + invoice.getQuantity());
+        System.out.println("Price per Item: " + invoice.getPricePerItem());
+        System.out.println("Invoice Amount: " + invoice.getInvoiceAmount());
     }
 }
